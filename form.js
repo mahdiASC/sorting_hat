1817a3bb198573affbc6711c9f5ef7cd4db50d1c
mahdiASC <mahdi@allstarcode.org>
2017-09-30 21:56:44 -0400

    testing form

--- form.js
+++ form.js
@@ -11,25 +11,30 @@
 // Spreadsheet  =1UYcjaOT7Oz5zoDzYSHLeLyRSJ8-JvV4qMwQJOzzioe8
 // secret = Ay8WDdRqPmGgMv3iCMUWJsM
 
-let url = "https://docs.google.com/forms/d/1B0ju0fHxoM8DF3GjS_mvY3zzQFwkq8iF-Dwv9CA-Q_w";
-function sendData(){
-    let name = $("#name").val();
-    let email =  $("#email").val();
-    let grade = $("input[type='radio'][name='grade']:checked").val();
-    let data = {
+// Where it starts
+// https://github.com/heaversm/google-custom-form
+/* <input type="hidden" name="entry.281452218" */
+// https://docs.google.com/forms/u/1/d/e/1FAIpQLSdenq8Yy8kUlAAl_EBXySWGWFtP3pGRUSkF8WloELiTPl0wzw/formResponse
+$('#form').one('submit',function(){
+    
+    let url = "https://docs.google.com/forms/u/1/d/e/1FAIpQLSdenq8Yy8kUlAAl_EBXySWGWFtP3pGRUSkF8WloELiTPl0wzw/formResponse?";
+    var q1ID = "entry.1576322846";
+    let name = encodeURIComponent($("#name").val());
 
-    };
-    $.ajax({
-        url: url,
-        data: data,
-        type: "post",
-        dataType: "xml",
-        statusCode: {
-            0 : clearAll
-        }
+    var q2ID = "entry.281452218";
+    let email =  encodeURIComponent($("#email").val());
 
-    })
-}
+    var q3ID = "entry.175417006";
+    let grade = encodeURIComponent($("input[type='radio'][name='grade']:checked").val());
+
+    let submitRef = '&submit=-1382798946213553241';
+    
+    var submitURL = (url + q1ID + "=" + name + "&" + q2ID + "=" + email + "&" + q3ID + "=" + grade + submitRef);
+
+    $(this)[0].action=submitURL;
+    console.log(submitURL);
+});
+
 
 function clearAll(){
     let name = $("#name").val("");
