4a5d38555b34dc331c361912f8b848c3cfc58926
mahdiASC <mahdi@allstarcode.org>
2017-10-01 00:19:52 -0400

    working form

--- form.js
+++ form.js
@@ -5,39 +5,26 @@
 
 // final submit takes all stored data and submits to google form
 
-// api key = AIzaSyCbDnUm7XXOv54842Dq0Agh-8XyVXfenKA
-// client id = 520347494565-o7qmirtc2nems9k0qdvu7uc6k1vaiuif.apps.googleusercontent.com
-
-// Spreadsheet  =1UYcjaOT7Oz5zoDzYSHLeLyRSJ8-JvV4qMwQJOzzioe8
-// secret = Ay8WDdRqPmGgMv3iCMUWJsM
-
 // Where it starts
 // https://github.com/heaversm/google-custom-form
 /* <input type="hidden" name="entry.281452218" */
 // https://docs.google.com/forms/u/1/d/e/1FAIpQLSdenq8Yy8kUlAAl_EBXySWGWFtP3pGRUSkF8WloELiTPl0wzw/formResponse
-$('#form').one('submit',function(){
-    
-    let url = "https://docs.google.com/forms/u/1/d/e/1FAIpQLSdenq8Yy8kUlAAl_EBXySWGWFtP3pGRUSkF8WloELiTPl0wzw/formResponse?";
-    var q1ID = "entry.1576322846";
-    let name = encodeURIComponent($("#name").val());
+console.log("loaded");
+ 
+let url = "https://docs.google.com/forms/u/1/d/e/1FAIpQLSevLTlqoqrkH3bNYNIStWbvjyzptP6JJV5bZC8RJAuRuxw8xg/formResponse?";
+var q1ID = "entry.1466968445";
+let name = encodeURIComponent($(`input`).val());//can do $.next();
 
-    var q2ID = "entry.281452218";
-    let email =  encodeURIComponent($("#email").val());
+let submitRef = '&submit=-459362922166947359';
 
-    var q3ID = "entry.175417006";
-    let grade = encodeURIComponent($("input[type='radio'][name='grade']:checked").val());
+var submitURL = (url + q1ID + "=" + name + submitRef);
 
-    let submitRef = '&submit=-1382798946213553241';
-    
-    var submitURL = (url + q1ID + "=" + name + "&" + q2ID + "=" + email + "&" + q3ID + "=" + grade + submitRef);
+console.log(submitURL);
+$('#form')[0].action=submitURL;
 
-    $(this)[0].action=submitURL;
-    console.log(submitURL);
-});
-
-
-function clearAll(){
-    let name = $("#name").val("");
-    let email =  $("#email").val("");
-    let grade = $("input[type='radio'][name='grade']:checked").prop("checked",false);
+// https://docs.google.com/forms/u/1/d/e/1FAIpQLSevLTlqoqrkH3bNYNIStWbvjyzptP6JJV5bZC8RJAuRuxw8xg/formResponse?entry.1466968445=asdf&submit=-459362922166947359
+function reset(){
+    let name = $("input").first.val("");
+    // let email =  $("#email").val("");
+    // let grade = $("input[type='radio'][name='grade']:checked").prop("checked",false);
 }
