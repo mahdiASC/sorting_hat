f5270598d54d42f3b3f628fe17e0726786caf073
mahdiASC <mahdi@allstarcode.org>
2017-10-25 04:04:38 -0400

    valid test

--- spec/validate-spec.js
+++ spec/validate-spec.js
@@ -30,4 +30,54 @@ describe("validate", ()=>{
     it('should store value of Cohort.all in .cohorts',()=>{
         expect(myValidate.cohorts).toEqual([cohort1,cohort2]);
     });
+
+    describe("dupStudentsCheck",()=>{
+        it("should return false if no duplicate students",()=>{
+            expect(myValidate.dupStudentsCheck()).toBeFalsy();
+        })
+
+        it("should return true if duplicate students exist",()=>{
+            cohort1 = {
+                "name":"cohort1",
+                "class":[student1,student2]
+            }
+            cohort2 = {
+                "name":"cohort2",
+                "class":[student3,student1]
+            }
+            
+            let tempVal = new validate(
+                [cohort1,cohort2],
+                [student1,student2,student3]
+            );
+            expect(tempVal.dupStudentsCheck()).toBeTruthy();
+        })
+    })
+
+    describe("getDups",()=>{
+        it("should return empty array when no duplicate students",()=>{
+            expect(myValidate.getDups()).toEqual([]);
+        })
+
+        it("should return array of duplicate students",()=>{
+            cohort1 = {
+                "name":"cohort1",
+                "class":[student1,student2]
+            }
+            cohort2 = {
+                "name":"cohort2",
+                "class":[student3,student1]
+            }
+            
+            let tempVal = new validate(
+                [cohort1,cohort2],
+                [student1,student2,student3]
+            );
+
+            expect(tempVal.getDups()).toEqual([student1]);
+        })
+        it("",()=>{
+            
+        })
+    })
 });
