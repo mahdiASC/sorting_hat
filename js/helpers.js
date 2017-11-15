403becb4cd4a55c1d66c4d4d0d62be3d81c7b605
mahdiASC <mahdi@allstarcode.org>
2017-11-15 15:34:55 -0800

    setting up popup

--- js/helpers.js
+++ js/helpers.js
@@ -20,6 +20,7 @@ var store_file = function (file, func) {
     });
 }
 
+//updating file paths
 $("div.fileUpload").each(function (x) {
     let main = $(this);
     let text = $(main.find(".file_text"));
@@ -40,3 +41,23 @@ let makeTextFile = function (file_name,text) {
     }));
     return text;
 };
+
+
+// setting up popouts
+$(function() {
+    //----- OPEN
+    $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+  
+        e.preventDefault();
+    });
+  
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+  
+        e.preventDefault();
+    });
+  });
